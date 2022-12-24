5b05e496f8e6e5bc7615ebb0e3749cbe714fa475
lex <lex.bolt@hotmail.com>
2022-12-24 11:18:43 +0100

    gpu unique ptr

--- Image_classifier/src/image_classifier.cpp
+++ Image_classifier/src/image_classifier.cpp
@@ -7,9 +7,10 @@
 int main() {
 
     std::unique_ptr<Dataset> dataset = std::make_unique<Dataset>("../dataset/cifar-10-batches-bin");
-    Gpu gpu("../src/kernels/test.cl");
-    if (gpu.gpu_enabled()) {
-        gpu.test();
+    std::unique_ptr<Gpu> gpu = std::make_unique<Gpu>("../src/kernels/test.cl");
+
+    if (gpu->gpu_enabled()) {
+        gpu->test();
     }
     dataset->display_all_images();
 
